d68e85b64e8e90bbf4df580151c7674f9c44ad3a
Jaimi Patel <jaimip.albiorix@gmail.com>
2025-01-23 18:42:03 +0530

    fe : create navigation file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import NavBar from '@/components/Navbar.vue';
 </script>
 
 <template>
+  <div>
+    <NavBar />
   <RouterView />
+  </div>
 </template>
